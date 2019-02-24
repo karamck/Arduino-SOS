1c4ff6af4bbf3d22a91d474afea3178807603963
karamck <34140604+karamck@users.noreply.github.com>
2019-02-24 12:19:41 -0500

    Add files via upload

---
+++ SOS.ino
@@ -0,0 +1,51 @@
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(7, OUTPUT);
+}
+
+
+void loop() {
+
+  //Blinking the first S
+  digitalWrite(7, HIGH);   
+  delay(500);                       
+  digitalWrite(7, LOW);    
+  delay(500);
+  digitalWrite(7, HIGH);   
+  delay(500);                       
+  digitalWrite(7, LOW);    
+  delay(500);
+  digitalWrite(7, HIGH);   
+  delay(500);                       
+  digitalWrite(7, LOW);    
+  delay(500);
+  
+  //Blinking the O
+  digitalWrite(7, HIGH);   
+  delay(1500);                       
+  digitalWrite(7, LOW);    
+  delay(500);
+  digitalWrite(7, HIGH);   
+  delay(1500);                      
+  digitalWrite(7, LOW);    
+  delay(500);
+  digitalWrite(7, HIGH);   
+  delay(1500);                       
+  digitalWrite(7, LOW);    
+  delay(500);
+
+  //Blinking the second S
+  digitalWrite(7, HIGH);
+  delay(500);                       
+  digitalWrite(7, LOW);    
+  delay(100);
+  digitalWrite(7, HIGH);   
+  delay(500);                       
+  digitalWrite(7, LOW);    
+  delay(100);
+  digitalWrite(7, HIGH);   
+  delay(500);                       
+  digitalWrite(7, LOW);    
+  delay(1000);
+}
